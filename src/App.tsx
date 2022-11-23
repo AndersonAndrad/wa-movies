af2a11c0ae6f65744e53a31ca9d010aa56133418
AndersonAndrad <anderson_andrade_@outlook.com>
2022-11-22 21:00:59 -0300

    create side details style component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Flex } from '@chakra-ui/react';
 import Header from "./components/header/header";
 import MovieCard from "./components/movie-card/movie-card";
+import { SideDetails } from './components/side-details/side-details';
 
 export default function App() {
 
   return (
-    <Flex className="App" direction={'column'}>
+    <Flex className="App" direction={'column'} height='100vh' >
       <Header />
-      <MovieCard />
+      <Flex height='100%'>
+        <MovieCard />
+        <SideDetails />
+      </Flex>
     </Flex>
   )
 }
